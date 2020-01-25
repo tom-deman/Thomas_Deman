1d087b08fc79549e7a68ab8d59d410369c116440
Thomas <dt.deman.thomas@gmail.com>
2020-01-25 15:24:52 +0100

    starting the application with main system and nav

--- src/App.jsx
+++ src/App.jsx
@@ -13,61 +13,97 @@ import './assets/css/font-awesome.css'
 import './assets/css/animate.css'
 import './assets/sass/style.sass'
 
+import Inquietude from './components/Inquietude'
 
-const App = (() => {
 
-    const linksTabs = [
-        'In Quietude',
-        'Modernity',
-        'Exhibition',
-        'Disorder',
-        'Contact'
-    ]
-
-    const galleryName = [
+const App = () => {
+    const [galleryYears, setGalleryYears] = useState([
         '"In Quietude 2016-2019"',
         '"Modernity 2015-2016"',
-        '"Exhibition 2017"',
-        '"Disorder 2018-2019"'
+        '"Disorder 2018-2019"',
+        '"Exhibition 2017"'
+    ])
+    
+    const Main = () => {
+        return <div className="imgMain h-full w-full"></div>
+    }
+
+    const linksTab = [
+        'Inquietude',
+        'Modernity',
+        'Disorder',
+        'Exhibition',
+        'Contact',
     ]
 
     const hover = 'hover:text-gray-700'
 
+
 return(
     <StrictMode>
         <div className="flex w-screen h-screen">
-            <div className="w-1/3 pl-12">
+            <Router>
 
-                <h1 className={`mt-12 text-3xl font-thin ${hover}`}>
-                    Thomas Deman
-                </h1>
+                <div className="w-1/3 pl-12">
 
-                <ul className="mt-12 font-thin pl-2">
-                    {linksTabs.map(
-                        (e, i) => {
-                            if(i === 3)
+                    <Link to="/">
+                        <h1 className={`mt-12 text-3xl font-thin ${hover}`}>
+                            Thomas Deman
+                        </h1>
+                    </Link>
+
+                    <ul className="mt-12 pl-2">
+                        { linksTab.map(
+                            (e, i) => {
+                                if(i === 3)
+                                    return (
+                                        <li 
+                                            key={i} 
+                                            className={`my-8 font-thin ${hover}`} 
+                                        >
+                                            <Link to={`/${e}`} >
+                                                {e}
+                                            </Link>
+                                        </li>
+                                    )
                                 return (
-                                    <li className={`my-8 ${hover}`}>
-                                        {e}
+                                    <li 
+                                        className={`font-thin ${hover}`} 
+                                        key={e}
+                                    >
+                                        {i === 0 ? 
+                                            <Link to={`/${e}`} >
+                                                In Quietude
+                                            </Link> :
+
+                                            <Link to={`/${e}`} >
+                                                {e}
+                                            </Link>
+                                        }
                                     </li>
                                 )
-                            return (
-                                <li className={hover}>
-                                    {e}
-                                </li>
-                            )
-                        }
-                    )}
-                </ul>
+                            }
+                        )}
+                    </ul>
 
-                <p className="font-thin text-xs mt-64 pl-2">{galleryName[0]}</p>
-            </div>
+                    <p className="font-thin text-xs mt-64 pl-2">{galleryYears[0]}</p>
+                </div>
 
-            <div className="w-2/3 imgMain"></div>
+
+                <div className="w-2/3">
+                    <Switch>
+                        <Link to="/Inquietude">
+                            <Route exact path="/" component={Main} />
+                        </Link>
+                        <Route path="/Inquietude" component={Inquietude} />
+                    </Switch>
+                </div>
+
+            </Router>
         </div>
     </StrictMode>
     )
-})
+}
 
 
 export default App
---
+++ src/components/Inquietude.jsx
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react'
+
+
+const Inquietude = () => {
+    return(
+        <StrictMode>
+            <h1>test</h1>
+        </StrictMode>
+    )
+}
+
+
+export default Inquietude 
