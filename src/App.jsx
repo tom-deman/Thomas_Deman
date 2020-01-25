0407c449e92b686eceb2aae96a63d5cf0dce8a39
Thomas <dt.deman.thomas@gmail.com>
2020-01-25 12:36:35 +0100

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, {
+    useState,
+    StrictMode
+} from 'react'
+
+
+import './assets/css/tailwind.css'
+import './assets/sass/style.sass'
+
+
+const App = (() => {
+
+return(
+    <StrictMode>
+
+    </StrictMode>
+    )
+})
+
+
+export default App
