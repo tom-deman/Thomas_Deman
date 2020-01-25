936181cc8ea3ba074dffb7af736aa8e4de0eee11
Thomas <dt.deman.thomas@gmail.com>
2020-01-25 13:39:47 +0100

    building structure, cut in components later

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,60 @@ import { Switch } from 'react-router'
 import './assets/css/tailwind.css'
 import './assets/css/font-awesome.css'
 import './assets/css/animate.css'
-import './assets/css/fonts.css'
 import './assets/sass/style.sass'
 
 
 const App = (() => {
 
+    const linksTabs = [
+        'In Quietude',
+        'Modernity',
+        'Exhibition',
+        'Disorder',
+        'Contact'
+    ]
+
+    const galleryName = [
+        '"In Quietude 2016-2019"',
+        '"Modernity 2015-2016"',
+        '"Exhibition 2017"',
+        '"Disorder 2018-2019"'
+    ]
+
+    const hover = 'hover:text-gray-700'
+
 return(
     <StrictMode>
-        
+        <div className="flex w-screen h-screen">
+            <div className="w-1/3 pl-12">
+
+                <h1 className={`mt-12 text-3xl font-thin ${hover}`}>
+                    Thomas Deman
+                </h1>
+
+                <ul className="mt-12 font-thin pl-2">
+                    {linksTabs.map(
+                        (e, i) => {
+                            if(i === 3)
+                                return (
+                                    <li className={`my-8 ${hover}`}>
+                                        {e}
+                                    </li>
+                                )
+                            return (
+                                <li className={hover}>
+                                    {e}
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
+
+                <p className="font-thin text-xs mt-64 pl-2">{galleryName[0]}</p>
+            </div>
+
+            <div className="w-2/3 imgMain"></div>
+        </div>
     </StrictMode>
     )
 })
