4820c8a7dca40b9d351759bc4124fb32845c4067
Thomas <dt.deman.thomas@gmail.com>
2020-01-25 13:01:20 +0100

    setting up others assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import React, {
     StrictMode
 } from 'react'
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Switch } from 'react-router'
 
 
 import './assets/css/tailwind.css'
 import './assets/css/font-awesome.css'
+import './assets/css/animate.css'
+import './assets/css/fonts.css'
 import './assets/sass/style.sass'
 
 
@@ -16,7 +19,7 @@ const App = (() => {
 
 return(
     <StrictMode>
-
+        
     </StrictMode>
     )
 })
