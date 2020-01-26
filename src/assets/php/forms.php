edb93605f9c6d666e0ba2f43f187bf26e0b6479d
Thomas <dt.deman.thomas@gmail.com>
2020-01-26 14:42:16 +0100

    add forms

--- src/assets/php/forms.php
+++ src/assets/php/forms.php
@@ -2,30 +2,24 @@
 
 <?php
 
-
 // Mail destination
 $destination = 'dt.deman.thomas@gmail.com';
-
 
 // Errors var
 $message_send = "Your message has been successfully sent !";
 $message_error = "Error <a href=\"../html/contact.html\"> come back to try again</a>";
 $message_invalid = "Error, please make sure all required fields are filled out correctly";
 
-
 // check if the var return isn't NULL
 if (!isset($_POST['send'])) {
 	echo '<p class="php_p">'.$message_error.'</p>'."\n";
 }
 
-
 // If it isn't we enter here
 else {
-
 	// Function to convert special characters in html entity
 	function Rec($text) {
 		$text = htmlspecialchars(trim ($text), ENT_QUOTES);
-
 		if (1 === get_magic_quotes_gpc()) {
 			$text = stripslashes($text);
 		}
@@ -34,12 +28,10 @@ else {
 		return $text;
 	}
 
-
 	// Settings php var in relation with contact.html
 	$name    = (isset($_POST['name']))    ? Rec($_POST['name'])    : '';
 	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
 	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
-
 	
 	// Checking if the fields aren't empty and if the email is a valid address
 	if (($name != '') && ($email != '' && filter_var($email, FILTER_VALIDATE_EMAIL)) && ($message != '')) {
@@ -52,20 +44,16 @@ else {
 				'Content-Disposition: inline'. "\r\n" .
 				'Content-Transfer-Encoding: 7bit'." \r\n" .
 				'X-Mailer:PHP/'.phpversion();
-
 		// Targeting my email with his var
 		$target = $destination;
 
 		$num_emails = 0;
 		$tmp = explode(';', $target);
 
-
 		foreach($tmp as $email_destination) {
-
 			if (mail ($email_destination, '', $message, $headers))
 				$num_emails ++;
 		}
-
 		// Validation message
 		echo '<p class=""> <br />'.$message_send.' Back</p>';
 	}
@@ -75,5 +63,4 @@ else {
 		echo '<p class="php_p">'.$message_invalid.'<a href="../html/contact.html"> <br /> <br /> Try again</a></p>'."\n";
 	}
 }
-
 ?>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,87 @@
 import React, { StrictMode } from 'react'
 
 
-const Contact = () => {
-    return(
+const Contact = () => {
+    return (
         <StrictMode>
-            <h1>Contact</h1>
+                <div className="w-full mt-24 pl-32">
+                    <form id="contact" method="post" action="../assets/php/forms.php">
+                        <div className="">
+                            <label 
+                                htmlFor="name" 
+                                className="block font-thin mb-2 ml-1"
+                            >
+                                Name
+                            </label>
+                            <input 
+                                className="p-2 border border-gray-400 rounded w-3/4 mb-6" 
+                                type="text" 
+                                id="name" 
+                                name="name" 
+                                required 
+                            />
+                        </div>
+
+                        <div className="">
+                            <label 
+                                htmlFor="email" 
+                                className="block font-thin mb-2 ml-1"
+                            >
+                                Email
+                            </label>
+                            <input 
+                                className="p-2 border border-gray-400 rounded w-3/4 mb-6" 
+                                type="text" 
+                                id="email" 
+                                name="email" 
+                                required 
+                            />
+                        </div>
+
+                        <div className="">
+                            <label 
+                                htmlFor="message" 
+                                className="block mb-2 ml-1 font-thin"
+                            >
+                                Message
+                            </label>
+                            <textarea 
+                                className="border border-gray-400 rounded w-3/4" 
+                                id="message" 
+                                name="message" 
+                                rows="4" 
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <button 
+                                type="submit" 
+                                className="py-2 px-4 text-sm rounded bg-gray-200 mt-8 hover:bg-gray-300 font-thin text-center" 
+                                name="send"
+                            >
+                                Send
+                            </button>
+                        </div>
+                    </form>
+
+                    <div className="mt-16">
+                        <p className="text-sm font-thin mb-6">
+                            dt.deman.thomas@gmail.com
+                        </p>
+
+                        <a 
+                            href="mailto:dt.deman.thomas@gmail.com" 
+                            target="_top"
+                            className="font-thin hover:text-gray-600"
+                        >
+                            --> Send me an Email
+                        </a>
+                    </div>
+                    <p className="text-sm mt-12 font-thin">
+                        All photographs &copy; Thomas Deman 2019
+                    </p>
+                </div>
         </StrictMode>
     )
 }
