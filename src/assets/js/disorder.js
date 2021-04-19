9418c4c9946d2cb6deddb0a54027cc967c6fa64d
tom-deman <49947427+tom-deman@users.noreply.github.com>
2021-04-19 17:15:58 +0200

    adding one new picture into disorder gallery

--- src/assets/js/disorder.js
+++ src/assets/js/disorder.js
@@ -5,6 +5,7 @@ import disorder4 from '../img/disorder/img4.jpg'
 import disorder5 from '../img/disorder/img5.jpg'
 import disorder6 from '../img/disorder/img6.jpg'
 import disorder7 from '../img/disorder/img7.jpg'
+import disorder8 from '../img/disorder/img8.jpg'
 
 
 let tabDisorder = [
@@ -14,7 +15,8 @@ let tabDisorder = [
      disorder4,
      disorder5,
      disorder6,
-     disorder7
+     disorder7,
+     disorder8
 ]
 
 
